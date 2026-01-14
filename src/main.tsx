4df6fe9125acc1d8d9943986410c2f4fc816fa51
LaurenLately <159574168+laurenLately@users.noreply.github.com>
2026-01-14 11:37:41 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// If you add a CSS file, import it here:
-// import "./index.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
