cf00d8f9240aa02d62fa4b0d4444c2275a45f295
LaurenLately <159574168+laurenLately@users.noreply.github.com>
2026-01-23 13:27:36 -0800

    Update index.js

--- api/health/index.js
+++ api/health/index.js
@@ -1,1 +1,11 @@
-
+module.exports=asynce function (context, req) {
+  context.res = {
+    status: 200,
+    headers: { "Content-Type": "application/json"},
+    body: {
+      ok: true,
+      service: "DJUS-Forecast-Ladders API",
+      timestamp: new Date().toISOString()
+    }
+  };
+};
